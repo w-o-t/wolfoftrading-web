6cd5f4fab7e0d576b23a3a2156adc0dd143bce3f
unknown <kontakt@markusschiel.de>
2026-02-22 20:31:03 +0100

    initial Web Setup

--- src/app/account/premium/premium-content.tsx
+++ src/app/account/premium/premium-content.tsx
@@ -14,19 +14,19 @@ export default function PremiumPage() {
       title="Premium Trader"
       features={[
         "🔥 25/25 Realtime-Signale",
-                            "🔥 Equity-Kurve & Profi Analytics",
-                            " €/% , Long/Short- und Asset Filter, Headmap, ",
-                            " DD Analyse, Recovery Factor, Sharpe Ratio, Expectancy uvm.",
-                            "🔥 Statistik",
-                            " Highend- und UltraProp Analytics",
-                            " Winrate, Trades, Profit Factor, Max DrowDown uvm.",
-                            "🔥 Signal-Historie",
-                            " Datums-, Asset-, Statussuche.",
-                            "🔥 Tradedetails",
-                            " Entry, Stop, TP & Realtime-Charts",
-                            "🔥 Priorisierte Pushes von professionellen Tradern",
-                            "🔥 EA Copytrading 🔥",
-                            " automatisierter Handel über einen EA Robot",
+        "🔥 Equity-Kurve & Profi Analytics",
+        "    €/% , Long/Short- und Asset Filter, Headmap, ",
+        "    DD Analyse, Recovery Factor, Sharpe Ratio, Expectancy uvm.",
+        "🔥 Statistik",
+        "    Highend- und UltraProp Analytics",
+        "    Winrate, Trades, Profit Factor, Max DrowDown uvm.",
+        "🔥 Signal-Historie",
+        "    Datums-, Asset-, Statussuche.",
+        "🔥 Tradedetails",
+        "    Entry, Stop, TP & Realtime-Charts",
+        "🔥 Priorisierte Pushes von professionellen Tradern",
+        "🔥 EA Copytrading 🔥",
+        "    automatisierter Handel über einen EA Robot",
       ]}
       monthlyPriceId={process.env.NEXT_PUBLIC_PRICE_PREMIUM_MONTHLY!}
       yearlyPriceId={process.env.NEXT_PUBLIC_PRICE_PREMIUM_YEARLY!}
