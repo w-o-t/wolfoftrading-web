bb2d1de1ad453cddc16d3441361bc20b2b6bb822
unknown <kontakt@markusschiel.de>
2026-02-23 11:40:10 +0100

    changes

--- src/components/PlanCard.tsx
+++ src/components/PlanCard.tsx
@@ -97,7 +97,7 @@ const badgeAlt = isPremium
       </div>
        <div>
         <ul> </ul>
-        <ul className="plan-list-short"> * Preis mtl. ist mtl. und jährl. ist jährl. kündbar. | Aktion: 10% auf den Standard Trader jährl. / 20% Premium Trader jährl.</ul>
+        <ul className="plan-list-short"> * Preis mtl. = mtl. und jährl. = jährl. kündbar. | Aktion: -10% Standard Trader jährl. / -20% Premium Trader jährl.</ul>
       </div>
     </div>
    
