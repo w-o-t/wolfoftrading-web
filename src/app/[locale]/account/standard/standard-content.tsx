67cd00cb877c848e841c287b590d4dfd627e55fa
unknown <kontakt@markusschiel.de>
2026-02-23 16:14:16 +0100

    changes

--- src/app/[locale]/account/standard/standard-content.tsx
+++ src/app/[locale]/account/standard/standard-content.tsx
@@ -16,12 +16,12 @@ export default function PremiumPage() {
       type="standard"
       title="Standard Trader"
       features={[
-         "<Image src='images/check.png' width='22' height='22'/> 17/25 Realtime-Signale",
-         "<Image src='images/check.png' width='22' height='22'/> Equity-Kurve | €/%, Long/Short- und Asset Filter",
-         "<Image src='images/check.png' width='22' height='22'/> Statistik | Basis-Performance",
+         "17/25 Realtime-Signale",
+         "Equity-Kurve | €/%, Long/Short- und Asset Filter",
+         "Statistik | Basis-Performance",
          " Winrate, Trades & Advanced Analyse",
          " Profit Factor, Max DrowDown",
-         "<Image src='images/check.png' width='22' height='22'/> Tradedetails | Entry, Stop, TP",
+         "Tradedetails | Entry, Stop, TP",
       ]}
       monthlyPriceId={process.env.NEXT_PUBLIC_PRICE_STANDARD_MONTHLY!}
       yearlyPriceId={process.env.NEXT_PUBLIC_PRICE_STANDARD_YEARLY!}
