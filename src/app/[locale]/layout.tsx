45e858ec201970074ee04b7d6c850236be04637d
unknown <kontakt@markusschiel.de>
2026-02-28 20:44:33 +0100

    changes

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,14 +3,13 @@ import {ReactNode} from 'react';
 import {NextIntlClientProvider} from 'next-intl';
 import {getMessages} from 'next-intl/server';
 
-export default async function LocaleLayout({
-  children,
-  params: {locale}
-}: {
+type Props = {
   children: ReactNode;
-  params: {locale: string};
-}) {
+  params: Promise<{ locale: string }>;
+};
 
+export default async function LocaleLayout({children, params}: Props) {
+  const {locale} = await params;
   const messages = await getMessages();
 
   return (
